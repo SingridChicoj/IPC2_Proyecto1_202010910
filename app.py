c9fa34db4a8a4378552b9600e09e6f5cfc5ab747
Singrid Martinez <3403252160101@ingenieria.usac.edu.gt>
2023-09-01 00:30:34 -0600

    Commit

--- app.py
+++ app.py
@@ -129,11 +129,11 @@ def menu():
                 entrada = ""
                 menu()
 
-#menu()
-cargar()
+menu()
+#cargar()
 #lista_senales_temporal.recorrer_imprimir_patrones()
 #lista_senales_temporal.grafica_listaO()
 #lista_senales_temporal.grafica_listaP()
-lista_senales_temporal.calcular_patrones("Señal Gordita")
-lista_senales_temporal.escritura_xml()
+#lista_senales_temporal.calcular_patrones("Prueba1")
+#lista_senales_temporal.escritura_xml()
 #lista_senales_temporal.grafica_listaR()
